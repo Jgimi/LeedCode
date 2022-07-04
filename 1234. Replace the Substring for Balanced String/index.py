f90dd605cdd747bf0b22400d69b55f93bdd37ca8
Jgimi <karlen92@yandex.ru>
2022-07-05 01:16:36 +0300

    Решение через replace

--- 1234. Replace the Substring for Balanced String/index.py
+++ 1234. Replace the Substring for Balanced String/index.py
@@ -1,11 +1,32 @@
 def Replace_the_Substring_for_Balanced_String(string):
-    new_list = []
-    string = string.replace(' ','')
-    for i in range(0,len(string),4):
-        new_list.append(string[i:i+4])
+    temp_string = string.replace('Q','')
+    Q_cnt = len(string) - len(temp_string)
+    temp_string = string.replace('W','')
+    W_cnt = len(string) - len(temp_string)
+    temp_string = string.replace('E','')
+    E_cnt = len(string) - len(temp_string)
+    temp_string = string.replace('R','')
+    R_cnt = len(string) - len(temp_string)
+    lists = [Q_cnt,W_cnt,E_cnt,R_cnt]
+    # sorted(lists,reverse=True)
+    # minimum = lists[-1]
+    avg = sum(lists)/len(lists)
+    # first_two = lists[:2]
+    # cnt = 0
+    # for i in first_two:
+    #     while i > avg:
+    #         cnt += 1
+    #         i -=1
+    # return cnt
+    minimum = min(lists)
+    new_lists = []
+    for i in lists:
+        new_lists.append(i-minimum)
     cnt = 0
-    for i in new_list:
-        cnt += len(i) - len(set(i))
+    for i in new_lists:
+        if i>avg:
+            cnt += i-avg
+
     return cnt
 
 
@@ -14,6 +35,7 @@ def Replace_the_Substring_for_Balanced_String(string):
 
 
 
-s = "WWQQRRRRQRQQ"
+
+s = "QQWE"
 
 print(Replace_the_Substring_for_Balanced_String(s))
