5f94695a0e5d18ce15af7e360dc1ceac31e44794
Jgimi <karlen92@yandex.ru>
2022-07-05 01:52:40 +0300

    Решение не проходит проверку на leedcode. Честно говоря, не понимаю почему

--- 1234. Replace the Substring for Balanced String/index.py
+++ 1234. Replace the Substring for Balanced String/index.py
@@ -1,41 +1,30 @@
-def Replace_the_Substring_for_Balanced_String(string):
-    temp_string = string.replace('Q','')
-    Q_cnt = len(string) - len(temp_string)
-    temp_string = string.replace('W','')
-    W_cnt = len(string) - len(temp_string)
-    temp_string = string.replace('E','')
-    E_cnt = len(string) - len(temp_string)
-    temp_string = string.replace('R','')
-    R_cnt = len(string) - len(temp_string)
-    lists = [Q_cnt,W_cnt,E_cnt,R_cnt]
-    # sorted(lists,reverse=True)
-    # minimum = lists[-1]
+def Replace_the_Substring_for_Balanced_String(s):
+    lists = []
+    all_letters = ['Q', 'W', 'E', 'R']
+    cnt=0
+    for i in all_letters:
+        for j in s:
+            if i==j:
+                cnt+=1
+        lists.append(cnt)
+        cnt=0
+
     avg = sum(lists)/len(lists)
-    # first_two = lists[:2]
-    # cnt = 0
-    # for i in first_two:
-    #     while i > avg:
-    #         cnt += 1
-    #         i -=1
-    # return cnt
-    minimum = min(lists)
-    new_lists = []
+    cnt_for_new_list = 0
     for i in lists:
-        new_lists.append(i-minimum)
-    cnt = 0
-    for i in new_lists:
         if i>avg:
-            cnt += i-avg
+            cnt_for_new_list += i-avg
 
-    return cnt
-
-
-
+    return int(cnt_for_new_list)
+    # return lists
 
 
 
 
 
-s = "QQWE"
+
+
+
+s = "WWEQERQWQWWRWWERQWEQ"
 
 print(Replace_the_Substring_for_Balanced_String(s))
